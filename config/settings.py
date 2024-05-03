043377764ee7fc63f3642b215543e16248311962
Aslon <arslon@gmail.com>
2024-05-03 23:58:49 +0500

    Commit

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qk!2nbehn1@$#e5i4tb0affu()j1^qcutb#_r*b&+57xc*3j4e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,12 +123,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR /'static',
-]
 
-# import os
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
