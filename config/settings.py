0a1b7196a6e4b1b31988f9810b60eb6431968b49
Aslon <arslon@gmail.com>
2024-05-05 22:24:43 +0500

    Commit

--- config/settings.py
+++ config/settings.py
@@ -88,9 +88,12 @@ DATABASES = {
     }
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
